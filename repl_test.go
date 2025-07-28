fcbf925421968c797828fa8b2c8f4500bd6b2e32
Nick <nickfong@2017-041.rz.local>
2025-07-28 07:47:09 -0700

    Add cleanup function and unit tests

---
+++ repl_test.go
@@ -0,0 +1,50 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "	hello world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+		{
+			input:    "",
+			expected: []string{},
+		},
+		{
+			input:    " ",
+			expected: []string{},
+		},
+		{
+			input:    "  ",
+			expected: []string{},
+		},
+		{
+			input:    "\t7\n",
+			expected: []string{"7"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("Actual (%d) and expected (%d) lengths were not equal!", len(actual), len(c.expected))
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("Actual (%s) and expected (%s) words differ!", word, expectedWord)
+			}
+		}
+	}
+}
