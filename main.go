ceac6b038d12218a3d58115b4fb9247186fa076c
Nick <nickfong@2017-041.rz.local>
2025-07-29 19:05:50 -0700

    Add help and exit commands, and command registry

--- main.go
+++ main.go
@@ -8,11 +8,47 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getRegistry() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
 }
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+	for name, cmd := range getRegistry() {
+		fmt.Printf("%s: %s\n", name, cmd.description)
+	}
+	return nil
+}
+
 func main() {
+
 	reader := bufio.NewReader(io.Reader(os.Stdin))
 	scanner := bufio.NewScanner(reader)
 	for {
@@ -22,10 +58,13 @@ func main() {
 			panic(fmt.Sprintf("Error: %v", scanner.Err()))
 		}
 		cleanInput := cleanInput(scanner.Text())
-		firstWord := ""
-		if len(cleanInput) > 0 {
-			firstWord = cleanInput[0]
+
+		if len(cleanInput) == 0 {
+			fmt.Println("Please type a command.")
+		} else if command, exists := getRegistry()[cleanInput[0]]; !exists {
+			fmt.Println("Unknown command")
+		} else if err := command.callback(); err != nil {
+			fmt.Printf("Error executing command: %v\n", err)
 		}
-		fmt.Printf("Your command was: %s\n", firstWord)
 	}
 }
